dd2eaf6ded84e9f24610708dd56b8449bd369330
rajesh91shah <rajesh91shah@gmail.com>
2021-08-16 22:41:11 +0530

    initial push

---
+++ DefineValidation/DefineValidation/DLValidation.swift
@@ -0,0 +1,14 @@
+//
+//  DLValidation.swift
+//  DefineValidation
+//
+//  Created by apple on 16/08/21.
+//
+
+import Foundation
+
+public struct DLValidation {
+   public static func printLog() {
+        print("test framework")
+    }
+}
---
+++ DefineValidation/DefineValidation/DefineValidation.h
@@ -0,0 +1,18 @@
+//
+//  DefineValidation.h
+//  DefineValidation
+//
+//  Created by apple on 16/08/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DefineValidation.
+FOUNDATION_EXPORT double DefineValidationVersionNumber;
+
+//! Project version string for DefineValidation.
+FOUNDATION_EXPORT const unsigned char DefineValidationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DefineValidation/PublicHeader.h>
+
+
